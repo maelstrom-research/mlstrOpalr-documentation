197e22cb8c3fb2b17a1f7a24d52f71c4db4823e9
guifabre <guijoseph.fabre@gmail.com>
2023-08-30 16:16:44 -0400

    push first commit

---
+++ update_files.R
@@ -0,0 +1,23 @@
+
+library(harmonizR)
+library(madshapR)
+library(fabR)
+library(mlstrOpalr)
+library(fs)
+# usethis::use_pkgdown()
+
+devtools::document(roclets = c('rd', 'collate', 'namespace', 'vignette'))
+devtools::build_readme()
+devtools::build_rmd('NEWS.Rmd')
+fs::dir_delete("docs")
+pkgdown::build_site()
+
+fs::dir_delete("../mlstrOpalr-documentation/docs")
+fs::dir_copy(
+  "docs",
+  "../mlstrOpalr-documentation/docs",
+  overwrite = TRUE)
+
+# push to git
+
+harmonizR_help()
